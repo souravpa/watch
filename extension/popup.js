8043a94eee153d663e24b564e03a8cd7ef071de4
David <d.yang.1450@gmail.com>
2020-05-28 17:24:04 -0500

    add different view for joining session

--- extension/popup.js
+++ extension/popup.js
@@ -1,19 +1,33 @@
 'use strict';
 
-let start = document.getElementById('start');
-start.onclick = function() {
-  link.value = 'sharable link goes here';
-  document.getElementById('create').classList.add('hidden');
-  document.getElementById('invite').classList.remove('hidden');
-  chrome.tabs.executeScript({file: 'jquery.js'}, function() {
-    chrome.tabs.executeScript({file: 'controls.js'})
-  });
-};
+chrome.tabs.query({currentWindow: true, active: true}, function(tabs){
+  let url = new URL(tabs[0].url);
+  let sessionid = url.searchParams.get('watchsessionid');
+  console.log('id = ', sessionid);
 
-let copy = document.getElementById('copy');
-copy.onclick = function() {
-  var copyText = document.getElementById("link");
-  copyText.select();
-  copyText.setSelectionRange(0, 99999);
-  document.execCommand("copy");
-}
+  if (sessionid) {
+    document.getElementById('create').classList.add('hidden');
+    document.getElementById('session').classList.remove('hidden');
+    chrome.tabs.executeScript({file: 'jquery.js'}, function() {
+      chrome.tabs.executeScript({file: 'controls.js'})
+    });
+  } else {
+    let start = document.getElementById('start');
+    start.onclick = function() {
+      link.value = 'sharable link goes here';
+      document.getElementById('create').classList.add('hidden');
+      document.getElementById('invite').classList.remove('hidden');
+      chrome.tabs.executeScript({file: 'jquery.js'}, function() {
+        chrome.tabs.executeScript({file: 'controls.js'})
+      });
+    };
+  }
+
+  let copy = document.getElementById('copy');
+  copy.onclick = function() {
+    var copyText = document.getElementById("link");
+    copyText.select();
+    copyText.setSelectionRange(0, 99999);
+    document.execCommand("copy");
+  }
+});
