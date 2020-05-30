5211d33f2893740224bbbf97ff81432191edca83
David <d.yang.1450@gmail.com>
2020-05-30 15:03:15 -0500

    extension implementation finished

--- extension/controls.js
+++ extension/controls.js
@@ -1,68 +1,94 @@
-var siteTags = [
-  {
-    hostname: 'youtube.com',
-    playButton: '.ytp-play-button',
-    pauseButton: '.ytp-play-button',
-    playing: function () {
-      return $('.ytp-play-button').attr('title').includes("Pause");
-    },
-    slider: '.ytp-progress-list',
-    progress: function () {
-      let pb = $('.ytp-progress-bar');
-      return parseFloat(pb.attr('aria-valuenow')) / parseFloat(pb.attr('aria-valuemax'));
-    }
-  },
-  {
-    hostname: 'netflix.com',
-    playButton: '.button-nfplayerPlay',
-    pauseButton: '.button-nfplayerPause',
-    playing: function () {
-      return $('.button-nfplayerPause').length > 0;
-    },
-    slider: '.track',
-    progress: function () {
-      let pb = $('.scrubber-head');
-      return parseFloat(pb.attr('aria-valuenow')) / parseFloat(pb.attr('aria-valuemax'));
-    }
-  }
-];
-
-var site;
-
-function clickProgressBar(elem, progress) {
-  var rect = elem.getBoundingClientRect(),
-      posX = rect.left, posY = rect.top;
+if (window.watchcontrolsInjected != true) {
+  window.watchcontrolsInjected = true;
   
-  posX += progress * rect.width;
-  posY += rect.height / 2;
-  var click = new MouseEvent('mousedown', {bubbles: true, clientX: posX, clientY: posY});    
-  elem.dispatchEvent(click);
-  click = new MouseEvent('mouseup', {bubbles: true, clientX: posX, clientY: posY});    
-  elem.dispatchEvent(click);
-}
-
-function play() {
-  if (!site.playing()) {
-    $(site.playButton).click();
+  let host = "localhost:8080";
+  var siteTags = [
+    {
+      hostname: 'youtube.com',
+      playButton: '.ytp-play-button',
+      pauseButton: '.ytp-play-button',
+      playing: function () {
+        return $('.ytp-play-button').attr('title').includes("Pause");
+      },
+      slider: '.ytp-progress-list',
+      progress: function () {
+        let pb = $('.ytp-progress-bar');
+        return parseFloat(pb.attr('aria-valuenow')) / parseFloat(pb.attr('aria-valuemax'));
+      }
+    },
+    {
+      hostname: 'netflix.com',
+      playButton: '.button-nfplayerPlay',
+      pauseButton: '.button-nfplayerPause',
+      playing: function () {
+        return $('.button-nfplayerPause').length > 0;
+      },
+      slider: '.track',
+      progress: function () {
+        let pb = $('.scrubber-head');
+        return parseFloat(pb.attr('aria-valuenow')) / parseFloat(pb.attr('aria-valuemax'));
+      }
+    }
+  ];
+  
+  var site;
+  for (let tags of siteTags) {
+    if (window.location.hostname.includes(tags.hostname)) {
+      site = tags;
+      break;
+    }
   }
-}
 
-function pause() {
-  if (site.playing()) {
-    $(site.pauseButton).click();
-  }
+  let playButton = $(site.playButton);
+  let pauseButton = $(site.pauseButton);
+  let slider = $(site.slider);
+  
+  console.log("hello sir");
+  console.log(site);
+  console.log(site.playing(), site.progress());
+  
+  $.get("http://" + host + "/rand", function (data) {
+    var socket = new WebSocket('ws://' + host + "/chat/" + data);
+  
+    socket.onopen = function (event) {
+      socket.send('hello');
+    };
+    
+    socket.onmessage = function (event) {
+      console.log('Message from server ', event.data);
+      let parts = event.data.split(" ");
+      switch (parts[0]) {
+        case "sir":
+          $('#watcher-num').html(parts[1] + " user in session");
+          break;
+        case "play":
+          playButton.click();
+          break;
+        case "pause":
+          pauseButton.click();
+          break;
+        case "skip":
+          let progress = parseFloat(parts[1]);
+          let rect = slider[0].getBoundingClientRect(),
+              posX = rect.left, posY = rect.top;
+      
+          posX += progress * rect.width;
+          posY += rect.height / 2;
+          let click = new MouseEvent('mousedown', {bubbles: true, clientX: posX, clientY: posY});
+          slider.dispatchEvent(click);
+          click = new MouseEvent('mouseup', {bubbles: true, clientX: posX, clientY: posY});
+          slider.dispatchEvent(click);
+          break;
+        default:
+          break;
+      }
+    };
+  
+    playButton[0].addEventListener("click", () => socket.send("play"));
+    // some sites like netflix have different elements for play and pause
+    if (playButton[0] !== pauseButton[0]) {
+      pauseButton[0].addEventListener("click", () => socket.send("pause"));
+    }
+    slider[0].addEventListener("mousedown", () => socket.send("skip " + site.progress()));
+  });
 }
-
-function skip(progress) {
-  clickProgressBar($(site.slider)[0], progress);
-}
-
-for (let tags of siteTags) {
-  if (window.location.hostname.includes(tags.hostname)) {
-    site = tags;
-    break;
-  }
-}
-console.log("hello sir");
-console.log(site);
-console.log(site.playing(), site.progress());
--- extension/popup.js
+++ extension/popup.js
@@ -9,25 +9,27 @@ chrome.tabs.query({currentWindow: true, active: true}, function(tabs){
     document.getElementById('create').classList.add('hidden');
     document.getElementById('session').classList.remove('hidden');
     chrome.tabs.executeScript({file: 'jquery.js'}, function() {
-      chrome.tabs.executeScript({file: 'controls.js'})
+      chrome.tabs.executeScript({code: 'var sessionid = ' + sessionid + ';'}, function() {
+        chrome.tabs.executeScript({file: 'controls.js'})
+      });
     });
   } else {
     let start = document.getElementById('start');
     start.onclick = function() {
-      link.value = 'sharable link goes here';
       document.getElementById('create').classList.add('hidden');
+      
+      link.value = 'sharable link goes here';
       document.getElementById('invite').classList.remove('hidden');
+      let copy = document.getElementById('copy');
+      copy.onclick = function() {
+        var copyText = document.getElementById("link");
+        copyText.select();
+        copyText.setSelectionRange(0, 99999);
+        document.execCommand("copy");
+      }
       chrome.tabs.executeScript({file: 'jquery.js'}, function() {
         chrome.tabs.executeScript({file: 'controls.js'})
       });
     };
-  }
-
-  let copy = document.getElementById('copy');
-  copy.onclick = function() {
-    var copyText = document.getElementById("link");
-    copyText.select();
-    copyText.setSelectionRange(0, 99999);
-    document.execCommand("copy");
   }
 });
