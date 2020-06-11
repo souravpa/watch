f94e96ca1a789cc4101a54d37f25314c6f1414b5
Ubuntu <ubuntu@ip-172-31-62-102.ec2.internal>
2020-06-11 22:47:21 +0000

    did something last time not really sure what but should commit it

--- server/watch/chat/consumers.py
+++ server/watch/chat/consumers.py
@@ -2,6 +2,7 @@ import json
 from channels.generic.websocket import WebsocketConsumer
 
 # chat/consumers.py
+
 import json
 from asgiref.sync import async_to_sync
 from channels.generic.websocket import WebsocketConsumer
@@ -10,12 +11,8 @@ class ChatConsumer(WebsocketConsumer):
     def connect(self):
         print(self.scope['url_route']['kwargs'])
         self.room_name = self.scope['url_route']['kwargs']['sessionid']
-        self.room_group_name = 'chat_%s' % self.room_name
-        
-        try:
-            session = models.ServerID.objects.get(id=self.room_name)
-        except:
-            return
+        self.room_group_name = 'chat_%s' % self.room_name    
+        session = models.ServerID.objects.get(id=self.room_name)
         session.num_connected+=1
         session.save()
         # Join room group
@@ -29,7 +26,8 @@ class ChatConsumer(WebsocketConsumer):
             self.room_group_name,
             {
                 'type': 'chat_message',
-                'message': message
+                'message': message,
+                'event': 'sir'
             }
         )
 
@@ -37,16 +35,26 @@ class ChatConsumer(WebsocketConsumer):
 
     def disconnect(self, close_code):
         # Leave room group
-        try: 
-            session = models.ServerID.objects.get(id=self.room_name)
-            if (session.num_connected == 1):
-                print("removed")
-                session.delete()
-            else: 
-                session.num_connected-=1 
-                session.save()
-        except:
-            pass
+        message = "hihi"
+        session = models.ServerID.objects.get(id=self.room_name)
+        if (session.num_connected == 1):
+            print("removed")
+            message = 'sir %s' % session.num_connected
+            session.delete()
+        else:
+            session.num_connected-=1 
+            message = 'sir %s' % session.num_connected
+            session.save()
+        print(message)
+        async_to_sync(self.channel_layer.group_send)(
+            self.room_group_name,
+            {
+               'type':'chat_message',
+               'message':message, 
+               'event': 'sir'
+            }
+        )
+
         async_to_sync(self.channel_layer.group_discard)(
             self.room_group_name,
             self.channel_name
@@ -54,23 +62,25 @@ class ChatConsumer(WebsocketConsumer):
 
     # Receive message from WebSocket
     def receive(self, text_data):
-        text_data_json = json.loads(text_data)
-        message = text_data_json['message']
-
+        message = text_data
         # Send message to room group
         async_to_sync(self.channel_layer.group_send)(
             self.room_group_name,
             {
                 'type': 'chat_message',
-                'message': message
+                'message': message,
+                'event': "control", 
+                'client': self.channel_name
             }
         )
 
     # Receive message from room group
     def chat_message(self, event):
-        message = event['message']
-        print(message)
+        if (event['event'] == 'sir'):
+            message=event['message']
+            self.send(text_data=message)
+        else:
+            message = event['message']
         # Send message to WebSocket
-        self.send(text_data=json.dumps({
-            'message': message
-        }))
+            if (event["client"] != self.channel_name):
+                self.send(text_data=message)
